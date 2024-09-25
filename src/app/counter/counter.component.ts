67cc17abe98106a1a4c390ee7c3c0491960a5959
fuchis <fuchisfgo@gmail.com>
2024-09-25 17:09:48 -0600

    Added Click Counter Component Logic

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -2,8 +2,26 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-counter',
-  template: '<h1>Counter</h1>'
+  template: `
+    <h3> Counter {{ counter }}</h3>
+    <button (click)="incrementCounterBy(+1)"> Increment </button>
+    <button (click)="resetCounter()"> Reset </button>
+    <button (click)="decrementCounterBy(-1)"> Decrement </button>
+  `
 })
 export class CounterComponent {
+  public counter: number = 10;
+
+  public incrementCounterBy(incrementFactor: number): void {
+    this.counter += incrementFactor;
+  }
+
+  public decrementCounterBy(decrementFactor: number): void {
+    this.counter += decrementFactor;
+  }
+
+  public resetCounter(): void {
+    this.counter = 10;
+  }
 
 }
